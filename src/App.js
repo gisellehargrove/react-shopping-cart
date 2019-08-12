b3fe69e06bcc32fa532b90d7e33d5f274d585f5b
gisellehargrove <gisellehargrove@gmail.com>
2019-08-12 16:05:32 -0400

    wrap components with context provider

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+// Contexts
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContexts';
 
 function App() {
 	const [products] = useState(data);
@@ -19,19 +22,21 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
