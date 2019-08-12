085d1fe476da55218be244a00096ce9e4e83d2aa
gisellehargrove <gisellehargrove@gmail.com>
2019-08-12 15:28:44 -0400

    remove props data from Products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,14 +6,14 @@ import ProductContext from '../contexts/ProductContext';
 
 const Products = props => {
 	const { products, addItem } = useContext(ProductContext);
-	
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
